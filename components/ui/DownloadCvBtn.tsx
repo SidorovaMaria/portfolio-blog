3250b8274ab731430f85394d09a71b6dc4b76936
Maria Sidorova <sidmashav@icloud.com>
2025-11-11 18:24:13 +0000

    Updated CV

--- components/ui/DownloadCvBtn.tsx
+++ components/ui/DownloadCvBtn.tsx
@@ -2,25 +2,23 @@
 import { File } from "lucide-react";
 
 const DownloadCVBtn = () => {
-  const cvFileUrlDocx = "CV.docx"; // Replace with the actual URL of your CV
-  const cvFileUrlPDF = "CV.pdf"; // Replace with the actual URL of your CV
-  //  file
-  // Replace with your desired CV filename
-  const cvFileNameDocx = "Maria_Sidorova_CV.docx";
-  const cvFileNamePDF = "Maria_Sidorova_CV.pdf";
+  const cvFileUrlPlain = "Maria_Sidorova_CV.pdf";
+  const cvFileUrlFigma = "CV.pdf";
+  const cvFileNamePlain = "Maria_Sidorova_CV.pdf";
+  const cvFileNameFigma = "Maria_Sidorova_CV.pdf";
 
-  const handleDownloadDocx = () => {
+  const handleDownloadPlain = () => {
     const link = document.createElement("a");
-    link.href = cvFileUrlDocx;
-    link.download = cvFileNameDocx;
+    link.href = cvFileUrlPlain;
+    link.download = cvFileNamePlain;
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
   };
-  const handleDownloadPDF = () => {
+  const handleDownloadFigma = () => {
     const link = document.createElement("a");
-    link.href = cvFileUrlPDF;
-    link.download = cvFileNamePDF;
+    link.href = cvFileUrlFigma;
+    link.download = cvFileNameFigma;
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
@@ -29,17 +27,17 @@ const DownloadCVBtn = () => {
     <div className=" flex gap-4 w-full items-center justify-center">
       <div
         role="button"
-        onClick={handleDownloadDocx}
+        onClick={handleDownloadPlain}
         className="inline-block download-cv-btn mb-4 p-3 rounded-xl bg-linear-to-r from-primary/20 to-secondary/20 hover:from-primary/40 hover:to-secondary/40 cursor-pointer  hover:shadow-[3px_3px_0px] shadow-fg/50 transition-all"
       >
         <div className="flex items-center gap-2 ">
           <File size={24} className="text-primary" />
-          <span className="text-sm font-medium tracking-wider uppercase">Download CV (docx)</span>
+          <span className="text-sm font-medium tracking-wider uppercase">Download CV </span>
         </div>
       </div>
       <div
         role="button"
-        onClick={handleDownloadPDF}
+        onClick={handleDownloadFigma}
         className="inline-block download-cv-btn mb-4 p-3 rounded-xl bg-linear-to-r from-primary/20 to-secondary/20 hover:from-primary/40 hover:to-secondary/40 cursor-pointer  hover:shadow-[3px_3px_0px] shadow-fg/50 transition-all"
       >
         <div className="flex items-center gap-2 ">
