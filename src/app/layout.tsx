13e020b37927fea826ec6410696f2e477e1a0dc2
Maria Sidorova <sidmashav@icloud.com>
2025-11-19 06:56:49 +0000

    Change default theme to 'dark' in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,7 +30,7 @@ export default async function RootLayout({
       <body className={`${inter.variable} ${clashDisplay.variable} antialiased`}>
         <ThemeProvider
           attribute="class"
-          defaultTheme="system"
+          defaultTheme="dark"
           enableSystem
           disableTransitionOnChange
         >
